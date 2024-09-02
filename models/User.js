2fa8f12a73f465c2d3b3e7eecadb0be7f71a062c
Stephen Muñez <sjtmunez@gmail.com>
2024-09-02 13:32:24 +0800

    feat: jwt lifespan

--- models/User.js
+++ models/User.js
@@ -36,7 +36,7 @@ UserSchema.methods.createJWT = function () {
     { userId: this._id, name: this.name },
     process.env.JWT_SECRET,
     {
-      expiresIn: '30d',
+      expiresIn: process.env.JWT_LIFESPAN,
     }
   );
 };
