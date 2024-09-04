651f047188dd1b5a7d91978a95abac2cc645f0ae
Stephen Muñez <sjtmunez@gmail.com>
2024-09-04 16:12:55 +0800

    feat: attach auth to job routes

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 // connect DB
 const connectDB = require('./db/connect');
 
+// authentication
+const authMiddleware = require('./middleware/authentication');
+
 // routers
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
@@ -19,7 +22,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/jobs', jobsRouter);
+app.use('/api/v1/jobs', authMiddleware, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
