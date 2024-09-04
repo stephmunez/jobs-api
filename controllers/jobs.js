720d435891b6efcbf2af5a5cd7199c3552e107a0
Stephen Muñez <sjtmunez@gmail.com>
2024-09-04 16:25:55 +0800

    feat: attach user to job routes

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -7,7 +7,7 @@ const getJob = async (req, res) => {
 };
 
 const createJob = async (req, res) => {
-  res.json(req.user);
+  res.send(req.user);
 };
 
 const updateJob = async (req, res) => {
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,3 +1,4 @@
+const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const { UnauthenticatedError } = require('../errors');
 
@@ -13,7 +14,10 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     const { userId, name } = payload;
-    req.user = { userId, name };
+
+    const user = await User.findById(userId).select('-password');
+    req.user = user;
+
     next();
   } catch (error) {
     throw new UnauthenticatedError('Invalid Authentication');
